5e3d23d5db6e4f11b157cda25c7ed8170668be11
shadow_walker <shadowwalkersb@gmail.com>
2016-12-15 12:46:28 -0600

    add bld.bat, update recipe

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+SET builddir=%SRC_DIR%\..\..\build_dir
+
+rmdir /s /q %builddir%
+if errorlevel 1 exit 1
+mkdir  %builddir%
+if errorlevel 1 exit 1
+cd %builddir%
+if errorlevel 1 exit 1
+
+copy %SRC_DIR%\\contrib\\CMakeLists.txt %SRC_DIR%\\CMakeLists.txt
+
+cmake %SRC_DIR% -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+cmake --build %builddir% --config Release --target install
+if errorlevel 1 exit 1
