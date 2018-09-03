91f1b7cac246ccb4bfc9f616d1575e4663040e71
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-03 12:16:49 -0300

    fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas${SHLIB_EXT}
 
 if [ "$(uname)" == "Darwin" ]; then
     ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas.0${SHLIB_EXT}"
-elif [ "$(uname)" == "Linux" ]
+elif [ "$(uname)" == "Linux" ]; then
     ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas${SHLIB_EXT}.0"
 fi
 
