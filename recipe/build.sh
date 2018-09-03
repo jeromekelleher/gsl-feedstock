dff54c3224534c554098f9f236a61b540855160e
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-03 11:51:08 -0300

    update-cb3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX
+./configure --prefix=${PREFIX}  \
+            --host=${HOST}
 
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
 
-if [ "$(uname)" == "Darwin" ]
-then
-    rm "$PREFIX"/lib/libgslcblas.*
-    ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.dylib"
-    ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.0.dylib"
+rm "$PREFIX"/lib/libgslcblas.*
+ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas${SHLIB_EXT}"
+
+if [ "$(uname)" == "Darwin" ]; then
+    ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas.0${SHLIB_EXT}"
 elif [ "$(uname)" == "Linux" ]
-then
-    rm "$PREFIX"/lib/libgslcblas.*
-    ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so"
-    ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so.0"
+    ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas${SHLIB_EXT}.0"
 fi
 
+# We can remove this when we start using the new conda-build.
+find $PREFIX -name '*.la' -delete
