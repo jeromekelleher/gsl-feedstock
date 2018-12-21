93a91800a42ce41bc6d9a34efb95851bff87e802
Jerome Kelleher <jk@well.ox.ac.uk>
2018-12-21 12:23:08 +0000

    Use recommended GSL linkage for openblas.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
+ export LIBS="-lm -lcblas -lopenblas"
 ./configure --prefix=${PREFIX}  \
             --host=${HOST}
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
-
-rm "$PREFIX"/lib/libgslcblas.*
-ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas${SHLIB_EXT}"
-
-if [ "$(uname)" == "Darwin" ]; then
-    ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas.0${SHLIB_EXT}"
-elif [ "$(uname)" == "Linux" ]; then
-    ln -s "$PREFIX/lib/libopenblas${SHLIB_EXT}" "$PREFIX/lib/libgslcblas${SHLIB_EXT}.0"
-fi
 
 # We can remove this when we start using the new conda-build.
 find $PREFIX -name '*.la' -delete
